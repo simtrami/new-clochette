763bde7171619bd553f566a79cae3203571c9109
Clément T <clement.taverne@telecom-sudparis.eu>
2019-05-22 17:21:51 +0200

    Define bottles table migration
    Signed-off-by: Simtrami <clement.taverne@telecom-sudparis.eu>

---
+++ database/migrations/2019_05_12_184925_create_bottles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBottlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bottles', function (Blueprint $table) {
+            $table->unsignedBigInteger('article_id')->primary();
+            $table->unsignedDecimal('volume', 5, 3);
+            $table->boolean('is_returnable')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('article_id')->references('item_id')->on('articles')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bottles');
+    }
+}
